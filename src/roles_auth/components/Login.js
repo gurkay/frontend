5bc95cade002bf3b5b49cd608f394f2954222e79
gurkay <g_basyigit@hotmail.com>
2023-08-14 21:13:57 +0300

    added new codes

--- src/roles_auth/components/Login.js
+++ src/roles_auth/components/Login.js
@@ -81,37 +81,44 @@ const Login = () => {
     }
 
     return (
+        <>
+            <div>
+                <label>email: eve.holt@reqres.in</label>
+            </div>
+            <div>
+                <label>password: cityslicka</label>
+            </div>
+            <section>
 
-        <section>
-            <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
-            <h1>Sign In</h1>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="username">Username:</label>
-                <input
-                    type='text'
-                    id='username'
-                    ref={userRef}
-                    autoComplete='off'
-                    onChange={(e) => setUser(e.target.value)}
-                    value={user}
-                    required
-                />
-                <label htmlFor="password">Password:</label>
-                <input
-                    type='password'
-                    id='password'
-                    onChange={(e) => setPwd(e.target.value)}
-                    value={pwd}
-                    required
-                />
-                <button>Sign In</button>
-            </form>
-            <p>
-                Need an Acconunt? <br />
-                <span className="line"><a href="#">Sing Up</a></span>
-            </p>
-        </section>
-
+                <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+                <h1>Sign In</h1>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="username">Username:</label>
+                    <input
+                        type='text'
+                        id='username'
+                        ref={userRef}
+                        autoComplete='off'
+                        onChange={(e) => setUser(e.target.value)}
+                        value={user}
+                        required
+                    />
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type='password'
+                        id='password'
+                        onChange={(e) => setPwd(e.target.value)}
+                        value={pwd}
+                        required
+                    />
+                    <button>Sign In</button>
+                </form>
+                <p>
+                    Need an Acconunt? <br />
+                    <span className="line"><a href="#">Sing Up</a></span>
+                </p>
+            </section>
+        </>
     );
 }
 
